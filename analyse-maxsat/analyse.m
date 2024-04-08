3e4bed29c2f0732b8040c76ea525059a8eb462c7
Sander Verwimp <91965164+sanDer153@users.noreply.github.com>
2024-04-08 12:14:51 +0200

    final results

--- analyse-maxsat/analyse.m
+++ analyse-maxsat/analyse.m
@@ -30,7 +30,7 @@ zlabel("Mediaan uitvoeringstijd (in ns)");
 %   - Random 3-SAT: The Plot Thickens p. 249 (3. Experimental setup)
 
 clc; clear;
-data = readmatrix("../resultsVSC-singlecore-2/2d-graph-results-4.26.csv");
+data = readmatrix("../resultsVSC-singlecore-3/2d-graph-results-3.8.csv");
 order = data(:, 1);
 time = data(:, 2);
 
